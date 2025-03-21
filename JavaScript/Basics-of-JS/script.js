afda0b9bbda509c90c54ceb7644cfb9c46cf2a49
Ali Zain <alizain10166@gmail.com>
2025-03-21 21:47:14 +0100

    Finish setup and solve 1st question of assignments

---
+++ JavaScript/Basics-of-JS/script.js
@@ -0,0 +1,15 @@
+/* 1️⃣;  Age Category Message – Ask the user for their age. If they are 
+under 18, print “You are a minor.” If they are between 18 and 60, 
+print “You are an adult.” If they are above 60, print “You are a 
+senior citizen.” */
+
+/* var userAge = Number(prompt("Enter your age!"));
+if (userAge < 18) {
+  console.log("You are a minor");
+} else if (userAge >= 18 && userAge <= 60) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a senior citizen");
+} */
+
+// === 1st question finish!
