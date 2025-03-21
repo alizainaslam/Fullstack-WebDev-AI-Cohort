aaf5fb1e38e3da12043cf7c31931350b22d2992e
Ali Zain <alizain10166@gmail.com>
2025-03-21 21:54:17 +0100

    Finish 2nd question of assignments

--- JavaScript/Basics-of-JS/script.js
+++ JavaScript/Basics-of-JS/script.js
@@ -1,6 +1,6 @@
-/* 1️⃣;  Age Category Message – Ask the user for their age. If they are 
-under 18, print “You are a minor.” If they are between 18 and 60, 
-print “You are an adult.” If they are above 60, print “You are a 
+/* 1️⃣;  Age Category Message – Ask the user for their age. If they are
+under 18, print “You are a minor.” If they are between 18 and 60,
+print “You are an adult.” If they are above 60, print “You are a
 senior citizen.” */
 
 /* var userAge = Number(prompt("Enter your age!"));
@@ -13,3 +13,18 @@ if (userAge < 18) {
 } */
 
 // === 1st question finish!
+
+/* 2️⃣;  Even or Odd Sum – Take two numbers from the user using
+prompt(). If the sum of both numbers is even, print “Even Sum”;
+otherwise, print “Odd Sum.”*/
+
+/* var input1 = Number(prompt("Enter your 1st number"));
+var input2 = Number(prompt("Enter your 2nd number"));
+var sum = input1 + input2;
+if (sum % 2 == 0) {
+  console.log("Even Sum");
+} else {
+  console.log("Odd Sum");
+}*/
+
+// === 2nd question finish!
