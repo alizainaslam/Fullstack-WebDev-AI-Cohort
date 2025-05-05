a33e9bee09505b0f54a0ca3beb940e6aa5945cb2
Ali Zain <alizain10166@gmail.com>
2025-05-05 21:38:00 +0200

    solve ISBN assignment

--- DSA/Day-31-32/script.js
+++ DSA/Day-31-32/script.js
@@ -46,3 +46,72 @@ function automorphicNumber(n) {
     return false;
   }
 }
+
+// —Switch Case
+let dayNumber = Number(4);
+
+/* switch (dayNumber) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+
+  default:
+    if (isNaN(dayNumber) || dayNumber < 1 || dayNumber > 7)
+      console.log("Invalid Input");
+    break;
+} */
+
+// —do-while
+let uInput;
+/* do {
+  uInput = Number(1);
+  console.log("You entered " + uInput);
+} while (uInput !== 0); */
+
+// Q. Repeat hello
+let count = 1;
+/* do {
+  console.log("Hello");
+  count++;
+} while (count < 5); */
+
+// 1. ISBN Number
+
+let ISBNNumber = parseInt("0471958697");
+let copyOfISBN = ISBNNumber;
+let countOfNumber = 1;
+let sumOfISBN = 0;
+let isValidISBN = true;
+
+while (ISBNNumber > 0) {
+  countOfNumber++;
+  ISBNNumber = Math.floor(ISBNNumber / 10);
+}
+if (isNaN(ISBNNumber) || countOfNumber < 10) throw new Error("Invalid Input");
+
+while (countOfNumber > 0) {
+  sumOfISBN += Math.floor(copyOfISBN % 10) * countOfNumber;
+  copyOfISBN = Math.floor(copyOfISBN / 10);
+  countOfNumber--;
+}
+
+// if (sumOfISBN % 11 === 0) console.log(isValidISBN);
+// else console.log((isValidISBN = false));
