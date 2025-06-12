a81079b36639e34627ef5fe0042b88e3b11eb2ae
Ali Zain <alizain10166@gmail.com>
2025-06-12 22:38:00 +0200

    Solve both exercise of this lecture

--- DOM/Day-38/README.md
+++ DOM/Day-38/README.md
@@ -1,6 +1,6 @@
 # DOM Manipulation
 
-This is the 2nd lecutre of dom manipulation exercise where created a random color generator.
+This is the 2nd lecture of dom manipulation exercise where created a random color generator and guess the winner project.
 
 ## 🔹 Topics Covered:
 
@@ -8,3 +8,5 @@ This is the 2nd lecutre of dom manipulation exercise where created a random colo
 ✅ **Update content**,  
 ✅ **Add style**,  
 ✅ **Add event listener**,
+
+📌 **Live Demo:** [Click here](https://day-38-dom-manipulation.netlify.app/)
--- DOM/Day-38/script.js
+++ DOM/Day-38/script.js
@@ -1,10 +1,76 @@
 const colorGeneratorBtn = document.querySelector(".color-generator");
 const colorCard = document.querySelector(".color-card");
+const guessBtn = document.querySelector(".guess-btn");
+const winnerResult = document.querySelector(".result");
 
-function colorValue() {
-  return Math.floor(Math.random() * 256);
+function randomValue(length) {
+  return Math.floor(Math.random() * length);
 }
 
 colorGeneratorBtn.addEventListener("click", function () {
-  colorCard.style.backgroundColor = `rgb(${colorValue()},${colorValue()}, ${colorValue()} )`;
+  colorCard.style.backgroundColor = `rgb(${randomValue(256)},${randomValue(
+    256
+  )}, ${randomValue(256)} )`;
+});
+
+// Guess the winner team
+const teams = [
+  {
+    name: "MI",
+    primaryColor: "blue",
+    secondaryColor: "gold",
+  },
+  {
+    name: "CSK",
+    primaryColor: "yellow",
+    secondaryColor: "blue",
+  },
+  {
+    name: "RCB",
+    primaryColor: "red",
+    secondaryColor: "black",
+  },
+  {
+    name: "KKR",
+    primaryColor: "purple",
+    secondaryColor: "gold",
+  },
+  {
+    name: "DC",
+    primaryColor: "blue",
+    secondaryColor: "red",
+  },
+  {
+    name: "RR",
+    primaryColor: "pink",
+    secondaryColor: "blue",
+  },
+  {
+    name: "SRH",
+    primaryColor: "orange",
+    secondaryColor: "black",
+  },
+  {
+    name: "LSG",
+    primaryColor: "lightblue",
+    secondaryColor: "orange",
+  },
+  {
+    name: "GT",
+    primaryColor: "darkblue",
+    secondaryColor: "gold",
+  },
+  {
+    name: "PBKS",
+    primaryColor: "red",
+    secondaryColor: "gold",
+  },
+];
+
+guessBtn.addEventListener("click", function () {
+  const team = randomValue(teams.length);
+  const { name, primaryColor, secondaryColor } = teams[team];
+  winnerResult.textContent = name;
+  winnerResult.style.backgroundColor = primaryColor;
+  winnerResult.style.color = secondaryColor;
 });
