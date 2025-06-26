d99bb16a3bdd9cc3942422fd3f1442d75dcbe0a6
Ali Zain <alizain10166@gmail.com>
2025-06-26 21:20:56 +0200

    solve same question in better way

--- DSA/Day-40-41/script.js
+++ DSA/Day-40-41/script.js
@@ -1,52 +1,53 @@
 // Q. Array left rotation by K elements
 let arrEle = [1, 2, 3, 4, 5];
-let kEle = 4 % arrEle.length;
-let remEle = [];
-let i = 0,
-  j = 0,
-  k = 0;
-while (i < kEle) {
-  remEle[i] = arrEle[i];
-  i++;
-}
-while (j < arrEle.length - kEle) {
-  arrEle[j] = arrEle[j + kEle];
-  j++;
-}
-while (k < remEle.length) {
-  arrEle[arrEle.length - kEle + k] = remEle[k];
-  k++;
-}
+let arrLength = arrEle.length;
+let kEle = 2 % arrLength;
 
-// Q. Linear Search an array - If element found print the index else -1
-const arr = [1, 2, 3, 4, 5, 6];
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
-    }
+function reverse(arr, start, end) {
+  let i = start,
+    j = end;
+  while (i < j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    i++;
+    j--;
   }
-  return -1;
+  return arr;
 }
-// console.log(linearSearch(arr, 4));
 
-// Q. — Binary Search
+reverse(arrEle, 0, arrLength - 1);
+reverse(arrEle, 0, arrLength - kEle - 1);
+reverse(arrEle, arrLength - kEle, arrLength - 1);
+
+// console.log(arrEle);
+
+// Q. Binary Searc
+let sortedArr = [1, 2, 3, 4, 5, 6];
 
 function binarySearch(arr, target) {
   let start = 0;
   let end = arr.length - 1;
-
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === target) return "target found";
+    else if (arr[mid] > target) end = mid - 1;
+    else start = mid + 1;
+  }
+  return "target not found";
+}
 
-    if (arr[mid] === target) {
-      return target;
-    } else if (arr[mid] > target) {
-      end = mid - 1;
-    } else {
-      start = mid + 1;
+// console.log(binarySearch(sortedArr, 5));
+
+// Q. Bubble sort
+let unSortedArr = [5, 2, 9, 1, 5, 6];
+for (let i = 0; i < unSortedArr.length - 1; i++) {
+  for (let j = 0; j < unSortedArr.length - i - 1; j++) {
+    if (unSortedArr[j] > unSortedArr[j + 1]) {
+      [unSortedArr[j], unSortedArr[j + 1]] = [
+        unSortedArr[j + 1],
+        unSortedArr[j],
+      ];
     }
   }
-  return -1;
 }
-// console.log(binarySearch(arr, 4));
+
+// console.log(unSortedArr);
