7c37236b529786c50f506cce0f9a2e3934ccaa72
Ali Zain <alizain10166@gmail.com>
2025-10-14 21:46:39 +0200

    feature: update feature create

--- ReactJs/Recipe/src/pages/RecipeDetail.jsx
+++ ReactJs/Recipe/src/pages/RecipeDetail.jsx
@@ -3,13 +3,78 @@ import { useNavigate, useParams } from "react-router-dom";
 import { recipeContext } from "../context/RecipeContext";
 import Button from "../components/Button";
 import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 const RecipeDetail = () => {
   const navigate = useNavigate();
 
+  const [isOpenUpdateForm, setIsOpenUpdateForm] = useState(false);
   const param = useParams();
   const { recipe, setRecipe } = useContext(recipeContext);
   const recdetail = recipe.find((recipe) => recipe.id == param.id);
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      image: recdetail?.image || "",
+      title: recdetail?.title || "",
+      description: recdetail?.description || "",
+      ingredients: recdetail?.ingredients || "",
+      chef: recdetail?.chef || "",
+      category: recdetail?.category || "",
+    },
+  });
+  const submitHandler = (data) => {
+    const index = recipe.findIndex((rec) => rec.id == param.id);
+    let copyData = [...recipe];
+    copyData[index] = { ...copyData[index], ...data };
+    setRecipe(copyData);
+
+    setIsOpenUpdateForm(false);
+    toast.success("Recipe has been updated!");
+  };
+
+  const updateRecipeForm = (
+    <form
+      className="bg-white rounded p-4 flex flex-col gap-6"
+      onSubmit={handleSubmit(submitHandler)}
+    >
+      <Input type="url" placeholder="image" {...register("image")} />
+
+      <Input type="text" placeholder="title" {...register("title")} />
+
+      <Input
+        type="text"
+        placeholder="description"
+        {...register("description")}
+      />
+
+      <Input
+        type="text"
+        placeholder="ingredients"
+        {...register("ingredients")}
+      />
+
+      <Input type="text" placeholder="chef-name" {...register("chef")} />
+
+      <select
+        className="bg-white border"
+        name="category"
+        {...register("category")}
+        required
+      >
+        <option value="" disabled>
+          Select Cuisine Type
+        </option>
+        <option value="italian">Italian</option>
+        <option value="mexican">Mexican</option>
+        <option value="asian">Asian</option>
+      </select>
+
+      <button className="bg-[#0b0b13] text-white px-3 py-2 rounded mt-3 cursor-pointer">
+        Update recipe
+      </button>
+    </form>
+  );
 
   const deleteHandler = () => {
     setRecipe(recipe.filter((reci) => reci.id !== recdetail.id));
@@ -18,7 +83,7 @@ const RecipeDetail = () => {
   };
 
   return (
-    <div className="mt-10 w-full">
+    <div className="mt-10 w-full relative">
       <h1 className="text-3xl font-semibold mb-7">Recipe Detail</h1>
 
       {recdetail ? (
@@ -34,13 +99,28 @@ const RecipeDetail = () => {
           </h3>
           <small>{recdetail.ingredients}</small>
           <p className="leading-snug">{recdetail.description}</p>
+          <small>{recdetail.category}</small>
         </div>
       ) : (
         ""
       )}
 
+      <div
+        className={
+          isOpenUpdateForm
+            ? "my-0 mx-auto md:w-[75%] lg:max-w-[550px] absolute top-17 left-0 right-0"
+            : "hidden"
+        }
+      >
+        {updateRecipeForm}
+      </div>
+
       <div className="mt-7 w-full flex justify-end gap-5">
-        <Button value="Update" cls="bg-blue-500" />
+        <Button
+          value="Update"
+          cls="bg-blue-500"
+          onClick={() => setIsOpenUpdateForm(!isOpenUpdateForm)}
+        />
         <Button value="Delete" cls="bg-red-500 " onClick={deleteHandler} />
       </div>
     </div>
